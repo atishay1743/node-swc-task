efddbc9322cc108087b7f4a3e2cef40854baf400
Atishay Jain <atishay1743@gmail.com>
2021-08-24 16:20:17 +0530

    port change

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.get('/:postId',function(req,res){
 })
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 9000;
 }
 app.listen(port, function() {
   console.log("Server started successfully");
